bfd78bc0932c8343a059a15b8da192757d47d8cf
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-11-07 00:48:06 +0300

    part-1
    part-1

---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,16 @@
+import React, { Suspense, lazy } from "react";
+
+//dynamic import
+const Cat = lazy(() => import("../../components/Cat"));
+
+const GeneralApp = () => {
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <Cat />
+      </Suspense>
+    </>
+  );
+};
+
+export default GeneralApp;
