9d2f5ba96245d710c697a0948c8704d8b4c557f5
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-11-09 23:26:01 +0300

    chats js Chat
    chats js Chat

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -6,10 +6,79 @@ import {
   InputBase,
   Button,
   Divider,
+  Avatar,
 } from "@mui/material";
+import Badge from "@mui/material/Badge";
 import { styled, alpha } from "@mui/material/styles";
 import { Archive, CircleDashed, MagnifyingGlass } from "phosphor-react";
 import React from "react";
+import { Faker } from "@faker-js/faker";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+const ChatElement = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        borderRadius: 1,
+        backgroundColor: "#fff",
+      }}
+      p={2}
+    >
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Stack direction={"row"} spacing={2}>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </StyledBadge>
+          <Stack spacing={0.3}>
+            <Typography variant="subtitle2">Messi</Typography>
+            <Typography variant="caption">How are you ?</Typography>
+          </Stack>
+        </Stack>
+        <Stack spacing={2} alignItems="center">
+          <Typography sx={{ fontWeight: 600 }}>9:36</Typography>
+          <Badge color="primary" badgeContent={2}></Badge>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -72,12 +141,17 @@ const Chats = () => {
             />
           </Search>
         </Stack>
-        <Stack>
+        <Stack spacing={1}>
           <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
             <Archive size={24} />
             <Button>Archive</Button>
           </Stack>
           <Divider />
+        </Stack>
+        <Stack>
+          <Stack direction={"column"}>
+            <ChatElement />
+          </Stack>
         </Stack>
       </Stack>
     </Box>
@@ -85,3 +159,5 @@ const Chats = () => {
 };
 
 export default Chats;
+
+// 2335
