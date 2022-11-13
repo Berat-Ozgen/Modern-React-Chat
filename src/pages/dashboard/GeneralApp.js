fcd90c108c8f28350adf3e3c4e43597b8a0f8b96
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-11-13 19:56:16 +0300

    Conversation #1
    Conversation #1

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,14 +1,25 @@
+import { Box, Stack } from "@mui/material";
 import React, { Suspense, lazy } from "react";
+import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
-
-//dynamic import
-const Cat = lazy(() => import("../../components/Cat"));
 
 const GeneralApp = () => {
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* Chats */}
       <Chats />
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: "#fff",
+        }}
+      >
+        {/* Conversation */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
